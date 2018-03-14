62c35171a6b5a18fec8cde1de7303b8fe29380eb
Igor Silva de Sousa <igorr201@hotmail.com>
2018-03-13 22:16:34 -0300

    Adição Campo de texto
    Adição campo de texto inicial usando native Library

--- CadastroCifra.lua
+++ CadastroCifra.lua
@@ -2,13 +2,49 @@ local composer = require( "composer" )
 local cifra = require( "Cifra" )
  
 local scene = composer.newScene()
+
+--local novaCifra = cifra:novaCifra()
  
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
+
+local fieldNome
+local fieldAutor
+local fieldInicio
+local fieldTom
+local fieldConteudo
  
+local function userImputNome( event )
  
+    if ( event.phase == "began" ) then
+        -- User begins editing "defaultField"
+ 
+    elseif ( event.phase == "ended" or event.phase == "submitted" ) then
+        -- Output resulting text from "defaultField"
+    cifra.nome = event.target.text  
+    print(cifra.nome)
+ 
+    elseif ( event.phase == "editing" ) then
+        
+    end
+end
+ 
+local function userImputAutor( event )
+ 
+    if ( event.phase == "began" ) then
+        -- User begins editing "defaultField"
+ 
+    elseif ( event.phase == "ended" or event.phase == "submitted" ) then
+        -- Output resulting text from "defaultField"
+    cifra.autor = event.target.text  
+    print(cifra.autor)
+ 
+    elseif ( event.phase == "editing" ) then
+        
+    end
+end
  
  
 -- -----------------------------------------------------------------------------------
@@ -19,11 +55,11 @@ local bg
 local title
 local button
 
---[[function mudarCena(event)
+function mudarCena(event)
 
-composer.gotoScene("Jogo", {effect = "slideLeft",time = 500})
+composer.gotoScene("Menu", {effect = "slideLeft",time = 500})
 
-end]]
+end
 
 -- create()
 function scene:create( event )
@@ -32,10 +68,10 @@ function scene:create( event )
     -- Code here runs when the scene is first created but has not yet appeared on screen
  
     bg = display.newRect(display  .contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
-    bg:setFillColor(1,1,1)
+    bg:setFillColor(0,0,0)
     sceneGroup:insert(bg)
 
-    title = display.newText("Cadastro Cifra", display.contentCenterX, display.contentCenterY/9, "Arial Black", 20)
+    title = display.newText("Cadastro Cifra", display.contentCenterX, display.contentCenterY/12, "Arial Black", 20)
     title:setFillColor(0, 0.5, 1 )
     sceneGroup:insert(title)
 
@@ -56,7 +92,28 @@ function scene:show( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
- 
+
+        local nome = display.newText("Nome da Música:", display.contentCenterX/2, display.contentHeight/7, "Arial Black", 16)   
+        nome:setFillColor(1,1,1)
+        sceneGroup:insert(nome)
+
+        fieldNome = native.newTextField( display.contentWidth/15, display.contentHeight/5.5, display.contentWidth*0.85, display.contentHeight/15 )
+        fieldNome.anchorX = 0
+        fieldNome.anchorY = 0
+        sceneGroup:insert(fieldNome)
+        fieldNome:addEventListener( "userInput", userImputNome )
+
+
+        local autor = display.newText("Autor:", display.contentCenterX/5, display.contentHeight/3.5, "Arial Black", 16)   
+        autor:setFillColor(1,1,1)
+        sceneGroup:insert(autor)
+
+        fieldAutor = native.newTextField( display.contentWidth/15, display.contentHeight/3, display.contentWidth*0.85, display.contentHeight/15 )
+        fieldAutor.anchorX = 0
+        fieldAutor.anchorY = 0
+        sceneGroup:insert(fieldAutor)
+        fieldAutor:addEventListener( "userInput", userImputAutor )
+
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
  
